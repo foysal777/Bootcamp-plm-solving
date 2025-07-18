3f5afea49a372b19993fe8be1d882d91a418301f
foysal777 <foysal.cse11@gmail.com>
2025-07-18 12:27:25 +0600

    add square pattern

---
+++ C_pattern_plm/square_pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "My pattern " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "*" ;
+        }
+        cout << endl ;
+        
+    }
+    // cout << " " << endl;
+}
